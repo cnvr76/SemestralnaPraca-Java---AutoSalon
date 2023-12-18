e9669178c56c56624e9e5e6d3f62c80a6c9e7574
killrr <64957160+cnvr76@users.noreply.github.com>
2023-12-18 23:40:35 +0100

    Delete SemestralnaPraca directory

--- SemestralnaPraca/src/AbstractClasses/ElectricVehicle.java
+++
@@ -1,9 +0,0 @@
-package AbstractClasses;
-
-public interface ElectricVehicle {
-
-    int getBatteryPercentage();
-    void setBatteryPercentage(int amount);
-    double charge(int toBatteryPercentage);
-
-}
