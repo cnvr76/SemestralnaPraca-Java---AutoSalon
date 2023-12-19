85396f3bc0fb2d9ba2b00ac7593ae9780a1ac519
killrr <64957160+cnvr76@users.noreply.github.com>
2023-12-19 17:13:29 +0100

    Delete docs directory

--- docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"AbstractClasses"},{"l":"All Packages","u":"allpackages-index.html"},{"l":"EVehicles"},{"l":"MotorVehicles"},{"l":"OtherVehicles"},{"l":"UserInteractions"}];updateSearchResults();
