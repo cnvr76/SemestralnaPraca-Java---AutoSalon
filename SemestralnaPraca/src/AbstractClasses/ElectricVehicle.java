57217f292df7f2d062a8f46086d8c359c31b6041
killrr <64957160+cnvr76@users.noreply.github.com>
2023-12-19 16:59:48 +0100

    Update ElectricVehicle.java

--- SemestralnaPraca/src/AbstractClasses/ElectricVehicle.java
+++ SemestralnaPraca/src/AbstractClasses/ElectricVehicle.java
@@ -4,6 +4,12 @@ public interface ElectricVehicle {
 
     int getBatteryPercentage();
     void setBatteryPercentage(int amount);
+
+    /**
+     *
+     * @param toBatteryPercentage - charges up to this level
+     * @return - returns the cost of the charge
+     */
     double charge(int toBatteryPercentage);
 
 }
