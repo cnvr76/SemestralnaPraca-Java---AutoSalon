e167214ed165b2036692882eb57d3afc087d614b
killrr <64957160+cnvr76@users.noreply.github.com>
2023-12-19 17:00:05 +0100

    Update FuelVehicle.java

--- SemestralnaPraca/src/AbstractClasses/FuelVehicle.java
+++ SemestralnaPraca/src/AbstractClasses/FuelVehicle.java
@@ -4,6 +4,12 @@ public interface FuelVehicle {
 
     double getFuelLevel();
     void setFuelLevel(double level);
+
+    /**
+     *
+     * @param fuelLevel - refuels up to this level
+     * @return - returns the cost of the refuel depending on the gas type
+     */
     double refuel(double fuelLevel);
     String getGearBox();
 
